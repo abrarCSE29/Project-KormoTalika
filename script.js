6a91827013c5127ce17bbcda693a1e534c6b6414
abrarCSE29 <jucse29.398@gmail.com>
2024-04-13 15:01:13 +0600

    addTask fucntion added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
- let task_heading = document.querySelector("#task-heading");
- let task_description = document.querySelector("#task-description");
- let task_add = document.querySelector("#new-task");
- let clear_task = document.querySelector("#clear-task");
+let task_heading = document.querySelector("#task-heading");
+let task_description = document.querySelector("#task-description");
+let task_add = document.querySelector("#new-task");
+let clear_task = document.querySelector("#clear-task");
 
- 
+function addTask() {
+    console.log(task_heading.value)
+  if (task_heading.value=== "") {
+    alert("Add a task heading");
+  }
+}
